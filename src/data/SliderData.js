93e00bd766e8b00b5df449a37c6d3296cf6bd95f
GithuiVictor <victormaina23@gmail.com>
2021-04-07 23:57:29 +0300

    feat: Hero Slider

---
+++ src/data/SliderData.js
@@ -0,0 +1,69 @@
+import ImageOne from '../images/House-1.jpg'
+import ImageTwo from '../images/House-2.jpg'
+import ImageThree from '../images/House-3.jpg'
+import ImageFour from '../images/House-4.jpg'
+import ImageFive from '../images/House-5.jpg'
+import ImageSix from '../images/House-6.jpg'
+import ImageSeven from '../images/House-7.jpg'
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa in Bali, Indonesia',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$2,860,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Calabasas, Califonia',
+        price: '$8,420,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$6,660,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFive,
+        alt: 'House'
+    },
+];
+
+export const SliderDataTwo = [
+    {
+        title: 'House in Las Vegas, Nevada',
+        price: '$8,980,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFive,
+        alt: 'House'
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$3,460,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageSix,
+        alt: 'House'
+    },
+    {
+        title: 'House in Alajuela, Costa Rica',
+        price: '$5,420,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageSeven,
+        alt: 'House'
+    },
+];
