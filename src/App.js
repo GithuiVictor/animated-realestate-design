938409538d5f143876044a57861e48f63a0b0de3
GithuiVictor <victormaina23@gmail.com>
2021-04-08 01:48:06 +0300

    feat: Dropdown Menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Dropdown from './components/Dropdown';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar'
 import GlobalStyle from './globalStyles';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <React.Fragment>
       <GlobalStyle/>
-      <Navbar/>
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Hero/>
     </React.Fragment>
   );
